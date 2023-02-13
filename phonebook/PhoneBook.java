5dcb435eb3e503ae7adc601df889b31b430b54dd
morozvitali <morozvitali99@gmail.com>
2023-02-14 00:36:39 +0200

    #####

--- phonebook/PhoneBook.java
+++ phonebook/PhoneBook.java
@@ -1,18 +1,70 @@
 package phonebook;
+// зробити не паблік
+//зробити окремий аплікейшн
+//видалити сміття
+
 import java.util.ArrayList;
 public class PhoneBook {
-    ArrayList<Record> phonebook = null;
-    private Record record;
-
-
-    public void add (String name, String number) {
-//        якщо (ім'я відсутнє) {
-//        створити новий Record
-//        додати новий Record у масив фонебук
+    public  ArrayList<Record> phonebook = new ArrayList<>();
+    public void add(String name, String number) {
+        phonebook.add(new Record(name, number));
     }
 
-    public void find (String name) {
+    @Override
+    public String toString() {
+        return "PhoneBook{" +
+                "phonebook=" + phonebook.toString() +
+                '}';
+    }
 
+    public Record find (String partName) {
+        for (Record rec : phonebook) {
+            if (rec.name.contains(partName)) {
+                return rec;
+            }
+        }
+        return null;
+    }
 
+    public ArrayList <Record> findAll (String partName) {
+        ArrayList <Record> findAllList = new ArrayList<>();
+        for (Record rec : phonebook) {
+            if (rec.name.contains(partName)) {
+                findAllList.add(rec);
+            }
+        }
+        return findAllList;
+    }
+
+    public static void main(String[] args) {
+        PhoneBook phonebook = new PhoneBook();
+        phonebook.add("енігма" , "21356745674");
+        phonebook.add("бетмен" , "25675744357557234");
+        phonebook.add("фриз" , "245674567557234");
+        phonebook.add("смайл" , "21344577234");
+        phonebook.add("джокер" , "21343574567234");
+
+        System.out.println(phonebook);
+
+        phonebook.find("бе");
+        System.out.println(phonebook.findAll("а"));
     }
 }
+
+class Record {
+    @Override
+    public String toString() {
+        return "Запис у книжці: " +
+                "Iм'я " + name + " Номер телефону " + numberList.toString();
+    }
+
+    public String name;
+    public ArrayList <String> numberList = new ArrayList<>();
+    //public String number;
+
+    public Record (String name, String number) {
+        this.name = name;
+        numberList.add (number);
+    }
+}
+
