1da2019237b4011eb0c1ed3fa754bbf9874fc814
morozvitali <80890701+morozvitali@users.noreply.github.com>
2023-02-13 17:55:24 +0200

    ####

--- phonebook/PhoneBook.java
+++ phonebook/PhoneBook.java
@@ -1,11 +1,18 @@
 package phonebook;
-
 import java.util.ArrayList;
-
 public class PhoneBook {
-    ArrayList phonebook = null;
+    ArrayList<Record> phonebook = null;
     private Record record;
-    public void add (Record record) {
-        phonebook.add(record);
+
+
+    public void add (String name, String number) {
+//        якщо (ім'я відсутнє) {
+//        створити новий Record
+//        додати новий Record у масив фонебук
+    }
+
+    public void find (String name) {
+
+
     }
 }
