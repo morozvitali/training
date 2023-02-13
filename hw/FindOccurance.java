5954305cae885f23a634e064a496ba7c07c7841f
morozvitali <80890701+morozvitali@users.noreply.github.com>
2023-02-13 16:59:05 +0200

    ###

--- hw/FindOccurance.java
+++ hw/FindOccurance.java
@@ -1,81 +1,65 @@
 package hw;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-/////////////////////////////////
-///////////////////////////// 4***
-///////////////////////////////////////
 
 public class FindOccurance {
+    ArrayList<String> arrayList;
 
+        public static HashMap findOccurance(ArrayList<String> arrayList) {
 
-    public static void calcOccurance(ArrayList<String> arrayList) {
+            HashMap dataResult = new HashMap<>(); // ініціалізуємо геш меп!
+            ArrayList<String> arrayListCopy = arrayList;
 
-        ArrayList <String> arrayListCopy = arrayList;
-
-        for (int i = 0; i < arrayList.size(); i++) {
-            System.out.print(arrayListCopy.get(i) + ": ");
-            int counter = 0;
-            for (int a = 0; a <arrayList.size(); a++) {
-                if (arrayListCopy.get(i).contains(arrayList.get(a))) {
-                    counter++;
+            for (int i = 0; i < arrayList.size(); i++) {
+                int counter = 0;
+                for (int a = 0; a < arrayList.size(); a++) {
+                    if (arrayListCopy.get(i).contains(arrayList.get(a))) {
+                        counter++;
+                    }
                 }
+
+                String value = arrayList.get(i);
+                dataResult.put(value, counter);
             }
-            arrayListCopy.removeAll(Collections.singleton(arrayList.get(i)));
-            System.out.println(counter);
+            return dataResult;
+        }
+
+        public static void main(String[] args) {
+            ArrayList <String> arrayList = new ArrayList<>();
+            arrayList.add("hi");
+            arrayList.add("Kitty");
+            arrayList.add("Kitty");
+            arrayList.add("Kitty");
+            arrayList.add("hi");
+            arrayList.add("bye");
+            arrayList.add("bye");
+            arrayList.add("bye");
+            arrayList.add("bye");
+            arrayList.add("bye");
+            arrayList.add("bye");
+            arrayList.add("hi");
+            arrayList.add("hi");
+            arrayList.add("hello");
+            arrayList.add("hello");
+            arrayList.add("hello");
+            arrayList.add("hello");
+            arrayList.add("hi");
+            arrayList.add("hello");
+            arrayList.add("hello");
+            arrayList.add("hello");
+            arrayList.add("hello");
+            arrayList.add("hello");
+            arrayList.add("hello");
+            arrayList.add("Kitty");
+            arrayList.add("Kitty");
+            arrayList.add("Kitty");
+            arrayList.add("Kitty");
+            arrayList.add("Kitty");
+
+            System.out.println(findOccurance(arrayList));
+
         }
     }
 
-    public static void main(String[] args) {
-        ArrayList <String> arrayList = new ArrayList<>();
 
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("dog");
-
-
-
-        arrayList.add("cat");
-        arrayList.add("cat");
-        arrayList.add("cat");
-        arrayList.add("cat");
-        arrayList.add("cat");
-        arrayList.add("cat");
-        arrayList.add("cat");
-        arrayList.add("cat");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("cat");
-        arrayList.add("fox");
-        arrayList.add("dog");
-        arrayList.add("dog");
-        arrayList.add("fox");
-        arrayList.add("fox");
-        arrayList.add("fox");
-        arrayList.add("fox");
-        arrayList.add("fox");
-        arrayList.add("fox");
-        arrayList.add("fox");
-        arrayList.add("fox");
-        arrayList.add("dog");
-        arrayList.add("fox");
-        arrayList.add("fox");
-        arrayList.add("fox");
-
-        calcOccurance (arrayList);
-
-
-    }
-
-}
